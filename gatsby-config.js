d913d72f60f06766114fb602d1d8b08882345d41
Corey Tegeler <coreytegeler@gmail.com>
2021-11-05 22:18:55 -0400

    new icons

--- gatsby-config.js
+++ gatsby-config.js
@@ -34,12 +34,24 @@ module.exports = {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `Follow Your Waste`,
-        short_name: `FYW`,
+        short_name: `Follow Your Waste`,
         start_url: `/`,
         background_color: `#ffffff`,
         theme_color: `#fc652c`,
         display: `standalone`,
-        icon: `src/images/icon.png`
+        icon: `src/images/icon.png`,
+        icons: [
+          {
+            src: `src/images/icon-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `src/images/icon-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+        ]
       }
     },
     // 'gatsby-plugin-offline'
